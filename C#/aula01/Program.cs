59cfb56960700d6e64768ed0734752b7b79094d3
AndreCampeloR <decocampeloreis@gmail.com>
2022-11-08 15:44:52 -0300

    Iniciando modulo de cSharp

---
+++ C#/aula01/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Digite seu nome:");
+
+string? nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade:");
+
+int idade = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite seu peso:");
+
+double peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("ola "+ nome +" incrivel você tem "+ idade+ " anos"+ " e seu peso é de "+peso+" kg ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string nome = "andré";
+
+// int idade = 20;
+
+// double peso = 52.2;
+
+// Console.WriteLine("seu nome é:"+ nome);
+// Console.WriteLine("sua idade é:"+ idade);
+// Console.WriteLine("seu peso é:"+ peso);
