7021a13b951d39789c31cd80f33aa0fc9be83c89
AndreCampeloR <decocampeloreis@gmail.com>
2022-11-09 14:48:33 -0300

    aula02

---
+++ C#/aula02/Program.cs
@@ -0,0 +1,12 @@
+﻿int acumulador = 0;
+int numero = 1;
+
+// for (int i = 1; i <= 10; i++)
+
+while (numero != 0)
+{
+    numero = Convert.ToInt32(Console.ReadLine());
+    acumulador = acumulador + numero;
+}
+
+Console.WriteLine($"A soma dos números é: {acumulador}");
