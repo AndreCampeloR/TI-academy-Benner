830dd6fd45c15697fd778c2f16498d4762ea27b2
AndreCampeloR <decocampeloreis@gmail.com>
2022-11-10 10:58:30 -0300

    Desafio C#

---
+++ C#/desafioC#/Program.cs
@@ -0,0 +1,38 @@
+﻿string volta = "Sim";
+do
+{
+double soma = 0;
+double aprovacao;
+Console.WriteLine("Digite a quantidade de notas a ser calculada");
+
+int notas = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= notas; i++)
+{
+Console.WriteLine("digite a " + i + "°" + " Nota:");
+
+double nota = Convert.ToInt32(Console.ReadLine());
+    
+soma += nota;
+}
+
+aprovacao = (soma / notas);
+
+Console.WriteLine("A média do aluno foi de: "+ aprovacao.ToString("F"));
+
+if (aprovacao >= 6)
+{
+    Console.WriteLine("Parabéns, Você passou!");
+} else
+{
+    Console.WriteLine("Que pena, Você reprovou!");
+}
+
+Console.WriteLine("Deseja calcular outra média? (Sim para calcular)");
+
+ volta = Convert.ToString(Console.ReadLine());
+
+} while (volta == "Sim");
+
+
+
